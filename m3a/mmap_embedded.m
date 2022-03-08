64cad54ba2c8834185ff7aa5172175991046a021
gcasale <g.casale@imperial.ac.uk>
2022-03-08 15:23:11 +0100

    refactor: scripts organized in subfolders

--- m3a/mmap_embedded.m
+++
@@ -1,7 +0,0 @@
-function Pc = mmap_embedded(MMAP)
-K = length(MMAP) - 2;
-Pc = cell(1,K);
-for k=1:K
-    Pc{k} = -MMAP{1} \ MMAP{2+k};
-end
-end
--- m3a/mmap_idc.m
+++
@@ -1,4 +0,0 @@
-function IDC = mmap_idc(MMAP)
-tinf = 1e6 / sum(mmap_lambda(MMAP));
-IDC = mmap_count_var(MMAP,tinf)/mmap_count_mean(MMAP,tinf);
-end
